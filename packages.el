584b336a97d5bb50e57b9daf5f14b534cbe51edd
Mohammad Anton Rizki Syahputra <moanrisy@gmail.com>
2023-05-21 19:43:02 +0700

    Add package: auto save, evil, nano theme

--- packages.el
+++ packages.el
@@ -18,7 +18,23 @@
     "g" '(:ignore t :wk "git")
     "gg" '(magit-status :wk "status")))
 
+(use-package real-auto-save
+  :init
+  (require 'real-auto-save)
+  (add-hook 'org-mode-hook 'real-auto-save-mode))
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
 ;; Always load theme as the last package! -----------------
 (straight-use-package
   '(nano-emacs :type git :host github :repo "rougier/nano-emacs"))
 (require 'nano)
+
+;; redisable menu bar mode after load nano theme
+(menu-bar-mode -1)
